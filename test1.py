d832926156b76d650bf714dbfc20cf0b0e153a7d
esthersudhagar <esthersudhagar@gmail.com>
2026-07-16 15:57:23 +0530

    Delete test1.py

--- test1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from streamlit import sidebar
-st.header("NoteBot")
-with sidebar:
-    st.title("Esther Notebot")
-    file=st.file_uploader("Upload notes PDF and start asking questions",type=["pdf"])
